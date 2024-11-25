e7b80eecea8ce2eac9e56e77d532c0acd477d401
Olga <minaeva9@gmail.com>
2024-11-25 15:30:45 -0500

    layout done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [require("daisyui")],
