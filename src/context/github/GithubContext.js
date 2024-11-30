3353225ba132a44a6e1d010b0eb5e4886d623540
Olga <minaeva9@gmail.com>
2024-11-29 20:39:17 -0500

    add GithubContext

---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,38 @@
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = "https://api.github.com";
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: false,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  //Get initial users (test)
+  const fetchUsers = async () => {
+    setLoading();
+    const response = await fetch(`${GITHUB_URL}/users`);
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  //Set loading
+  const setLoading = () => dispatch({ type: "SET_LOADING" });
+
+  return (
+    <GithubContext.Provider value={{ users: state.users, loading, fetchUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
