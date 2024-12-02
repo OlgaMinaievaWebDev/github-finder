2425cc6c2b9b43f81a3d64214e0c50186d0910bd
Olga <minaeva9@gmail.com>
2024-12-02 16:22:39 -0500

    user repos

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -7,6 +7,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -63,6 +64,35 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  //get user repos
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    try {
+      const response = await fetch(
+        `https://api.github.com/users/${login}/repos?${params}`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} - ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_REPOs",
+        payload: data,
+      });
+    } catch (error) {
+      console.error("Fetch Users Error:", error.message);
+    }
+  };
+
   //clear users from state
   const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
 
@@ -75,9 +105,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
       }}
     >
       {children}
