77e5a6474e9a36aa04a749c327736d671352d64e
Olga <minaeva9@gmail.com>
2024-12-04 14:35:53 -0500

    move getUsers to action file

--- src/context/github/GithubActions.jsx
+++ src/context/github/GithubActions.jsx
@@ -20,3 +20,48 @@ export const searchUsers = async (text) => {
     console.error("Fetch Users Error:", error.message);
   }
 };
+
+//get single user
+export const getUser = async (login) => {
+  try {
+    const response = await fetch(`https://api.github.com/users/${login}`);
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      return data;
+    }
+  } catch (error) {
+    console.error("Fetch Users Error:", error.message);
+  }
+};
+
+//get user repos
+export const getUserRepos = async (login) => {
+  const params = new URLSearchParams({
+    sort: "created",
+    per_page: 10,
+  });
+
+  try {
+    const response = await fetch(
+      `https://api.github.com/users/${login}/repos?${params}`
+    );
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} - ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Fetch Users Error:", error.message);
+  }
+};
--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -13,73 +13,11 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  //get single user
-  const getUser = async (login) => {
-    try {
-      const response = await fetch(`https://api.github.com/users/${login}`);
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
-      }
-
-      if (response.status === 404) {
-        window.location = "/notfound";
-      } else {
-        const data = await response.json();
-
-        dispatch({
-          type: "GET_USER",
-          payload: data,
-        });
-      }
-    } catch (error) {
-      console.error("Fetch Users Error:", error.message);
-    }
-  };
-
-  //get user repos
-  const getUserRepos = async (login) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      sort: "created",
-      per_page: 10,
-    });
-
-    try {
-      const response = await fetch(
-        `https://api.github.com/users/${login}/repos?${params}`
-      );
-
-      if (!response.ok) {
-        throw new Error(`Error: ${response.status} - ${response.statusText}`);
-      }
-
-      const data = await response.json();
-
-      dispatch({
-        type: "GET_REPOs",
-        payload: data,
-      });
-    } catch (error) {
-      console.error("Fetch Users Error:", error.message);
-    }
-  };
-
-  //clear users from state
-  const clearUsers = () => dispatch({ type: "CLEAR_USERS" });
-
-  // Set loading
-  const setLoading = () => dispatch({ type: "SET_LOADING" });
-
   return (
     <GithubContext.Provider
       value={{
         ...state,
         dispatch,
-        clearUsers,
-        getUser,
-        getUserRepos,
       }}
     >
       {children}
